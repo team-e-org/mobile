7ca8106337518b189b6c2b22946f0430db375e97
Naoto Ishida <naoto.ishida@dena.com>
2020-06-11 11:11:23 +0900

    Implement Pins API

--- lib/api/pins_api.dart
+++ lib/api/pins_api.dart
@@ -1,6 +1,58 @@
-abstract class PinsApi{
-  Future<List<PinModel>> pins();
-  Future<PinModel> pin(String id);
-  Future<bool> editPin(Pin pin);
-  Future<bool> deletePin(Pin pin);
+import 'dart:convert';
+
+import 'package:mobile/api/api_client.dart';
+import 'package:mobile/model/models.dart';
+
+class EditPin {
+  EditPin({
+    this.title,
+    this.description,
+    this.isPrivate,
+  });
+
+  final String title;
+  final String description;
+  final bool isPrivate;
+}
+
+abstract class PinsApi {
+  Future<List<Pin>> pins({int page});
+
+  Future<Pin> pin(int id);
+
+  Future<bool> editPin(EditPin pin);
+
+  Future<bool> deletePin(int id);
+}
+
+class DefaultPinsApi extends PinsApi {
+  DefaultPinsApi(this._client);
+
+  final ApiClient _client;
+
+  @override
+  Future<bool> deletePin(int id) async {
+    final response = await _client.delete("/pins/$id");
+    return response.statusCode == 204;
+  }
+
+  @override
+  Future<bool> editPin(EditPin pin) {
+    // TODO: API仕様変更中のためいったん保留
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Pin> pin(int id) async {
+    final response = await _client.get("/pins/$id");
+    return Pin.fromJson(jsonDecode(response.body));
+  }
+
+  @override
+  Future<List<Pin>> pins({int page = 1}) async {
+    final response = await _client.get("/pins?page=$page");
+    return (jsonDecode(response.body) as List)
+        .map((it) => Pin.fromJson(it))
+        .toList();
+  }
 }
