60b4af5cd083d0dcacd3d59d591ecd2811c2b055
Daigo Hayashi <d.h.9489@gmail.com>
2020-06-11 17:02:30 +0900

    implement homescreen

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/view/mock/mock_screen_common.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:mobile/model/models.dart';
+import 'package:mobile/view/components/components.dart';
 
 class HomeScreen extends StatelessWidget {
+  var pins = [Pin.fromMock(), Pin.fromMock(), Pin.fromMock(), Pin.fromMock()];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: MockScreenCommon(
-        name: 'Home screen',
+    return SafeArea(
+      child: Container(
+        child: PinGridView(pins: pins),
       ),
     );
   }
