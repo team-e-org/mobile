1d9dfd2af00f187f55be56917709143ac43cc1bc
Naoto Ishida <naoto.ishida@dena.com>
2020-06-11 11:22:43 +0900

    Implement User.fromJson

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -11,15 +11,19 @@ class User {
     this.icon,
   });
 
-  // TODO
-  // factory User fromJson(Map<String, dynamic> json){}
+  User.fromJson(Map<String, dynamic> json)
+      : id = json['id'] as int,
+        name = json['name'],
+        email = json['email'],
+        icon = json['icon'];
 
   static User fromMock() {
     return User(
       id: 123,
       name: "user name",
       email: "email@mail.com",
-      icon: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      icon:
+          "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
     );
   }
 }
