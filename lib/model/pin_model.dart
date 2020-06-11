4cad9bfbbe9ded4c8e1b0873c75e84a3743e15ba
Naoto Ishida <naoto.ishida@dena.com>
2020-06-11 10:30:27 +0900

    Implement Pin.fromJson

--- lib/model/pin_model.dart
+++ lib/model/pin_model.dart
@@ -17,8 +17,14 @@ class Pin {
     this.isPrivate,
   });
 
-  // TODO
-  // factory Pin fromJson(Map<String, dynamic> json){}
+  Pin.fromJson(Map<String, dynamic> json)
+      : id = json['id'] as int,
+        title = json['title'],
+        description = json['description'],
+        url = json['url'],
+        userId = json['userId'] as int,
+        imageUrl = json['imageUrl'],
+        isPrivate = json['isPrivate'] as bool;
 
   static Pin fromMock() {
     return Pin(
@@ -27,7 +33,8 @@ class Pin {
       description: "description",
       url: "http://www.sample.com",
       userId: 143,
-      imageUrl: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      imageUrl:
+          "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
       isPrivate: false,
     );
   }
