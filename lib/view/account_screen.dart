ee9034efea54cbe7f7874961e556f63617b5f049
Naoto Ishida <naoto.ishida@dena.com>
2020-06-11 16:42:56 +0900

    Add FAB to account screen

--- lib/view/account_screen.dart
+++ lib/view/account_screen.dart
@@ -4,9 +4,15 @@ import 'package:mobile/view/mock/mock_screen_common.dart';
 class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: MockScreenCommon(
+    return Scaffold(
+      body: MockScreenCommon(
         name: 'Account screen',
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // TODO 遷移する
+        },
       ),
     );
   }
