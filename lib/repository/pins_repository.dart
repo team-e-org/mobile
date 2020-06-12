eb5dc77818ec072f8e5fda7bc068e570b825115a
Daigo Hayashi <d.h.9489@gmail.com>
2020-06-12 14:47:18 +0900

    introduce pins api

--- lib/repository/pins_repository.dart
+++ lib/repository/pins_repository.dart
@@ -1,9 +1,12 @@
+import 'package:mobile/api/pins_api.dart';
+
 class PinsRepository {
-  factory PinsRepository() {
-    return _instance ?? PinsRepository._internal();
+  factory PinsRepository(PinsApi api) {
+    return _instance ?? PinsRepository._internal(api);
   }
 
-  PinsRepository._internal();
+  PinsRepository._internal(this._api);
 
   static PinsRepository _instance;
+  PinsApi _api;
 }
