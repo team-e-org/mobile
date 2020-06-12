8d38e1dc087430b809d5b20e2e0da51439a5130e
Naoto Ishida <naoto.ishida@dena.com>
2020-06-12 11:44:37 +0900

    Use pushReplacementNamed to 1-direction navigation

--- lib/view/pin_select_photo_screen.dart
+++ lib/view/pin_select_photo_screen.dart
@@ -4,9 +4,14 @@ import 'package:mobile/view/mock/mock_screen_common.dart';
 class PinSelectPhotoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: MockScreenCommon(
-        name: 'Pin select photo screen',
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select photo'),
+      ),
+      body: Container(
+        child: MockScreenCommon(
+          name: 'Pin select photo screen',
+        ),
       ),
     );
   }
