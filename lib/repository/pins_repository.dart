9d2ef6f4a3c17381a939799255808bef6a686497
Daigo Hayashi <d.h.9489@gmail.com>
2020-06-12 15:07:19 +0900

    add getHomePagePins function into PinsRepository

--- lib/repository/pins_repository.dart
+++ lib/repository/pins_repository.dart
@@ -1,4 +1,5 @@
 import 'package:mobile/api/pins_api.dart';
+import 'package:mobile/model/models.dart';
 
 class PinsRepository {
   factory PinsRepository(PinsApi api) {
@@ -9,4 +10,8 @@ class PinsRepository {
 
   static PinsRepository _instance;
   PinsApi _api;
+
+  Future<List<Pin>> getHomePagePins({int page}) async {
+    return _api.pins(page: page ?? 1);
+  }
 }
