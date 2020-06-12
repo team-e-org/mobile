5effaf833745731694d3263dc171ef722c39d2b2
Daigo Hayashi <d.h.9489@gmail.com>
2020-06-12 16:41:50 +0900

    implement bloc

--- lib/bloc/main_screen_bloc.dart
+++ lib/bloc/main_screen_bloc.dart
@@ -2,7 +2,12 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:http/http.dart';
+import 'package:mobile/api/api_client.dart';
+import 'package:mobile/api/pins_api.dart';
+import 'package:mobile/bloc/pins_api_stream.dart';
 import 'package:mobile/model/models.dart';
+import 'package:mobile/repository/repositories.dart';
 
 //////// Event ////////
 abstract class MainScreenEvent extends Equatable {
@@ -69,10 +74,49 @@ class Loading extends MainScreenState {
   }) : super(page: page, pins: pins);
 }
 
+//////// Bloc ////////
 class MainScreenBloc extends Bloc<MainScreenEvent, MainScreenState> {
-  @override
-  MainScreenState get initialState => null;
+  MainScreenBloc() {
+    _apiClient = ApiClient(Client(), apiEndpoint: _apiEndpoint);
+    _pinsRepository = PinsRepository(DefaultPinsApi(_apiClient));
+    _apiStreams = PinsApiStreams(_pinsRepository);
+  }
+
+  static const String _apiEndpoint = 'http://localhost:8080';
+  ApiClient _apiClient;
+  PinsRepository _pinsRepository;
+  StreamSubscription<dynamic> _apiSubscription;
+  PinsApiStreams _apiStreams;
 
   @override
-  Stream<MainScreenState> mapEventToState(MainScreenEvent event) async* {}
+  MainScreenState get initialState => InitialState();
+
+  @override
+  Stream<MainScreenState> mapEventToState(MainScreenEvent event) async* {
+    if (event is LoadPinsPage) {
+      mapLoadPinsPageToState(event);
+    } else if (event is FinishLoading) {
+      mapFinishLoadingToState(event);
+    }
+  }
+
+  Stream<MainScreenState> mapLoadPinsPageToState(LoadPinsPage event) async* {
+    if (state is Loading) {
+      return;
+    } else if (state is InitialState || state is DefaultState) {
+      await _apiSubscription.cancel();
+      _apiSubscription = _apiStreams.getHomePagePins(page: event.page).listen(
+          (pins) => add(FinishLoading(page: state.page, additionalPins: pins)));
+      yield Loading(page: state.page, pins: state.pins);
+    }
+  }
+
+  Stream<MainScreenState> mapFinishLoadingToState(FinishLoading event) async* {
+    if (state is Loading) {
+      await _apiSubscription.cancel();
+      final _pins = List<Pin>.from(state.pins)..addAll(event.additionalPins);
+      yield DefaultState(page: state.page, pins: _pins);
+    }
+    return;
+  }
 }
