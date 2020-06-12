0a2bf32301f07c910b173609f7910990d9f952ed
Naoto Ishida <naoto.ishida@dena.com>
2020-06-12 20:10:29 +0900

    temp

--- lib/view/root_screen.dart
+++ lib/view/root_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/view/account_screen.dart';
 import 'package:mobile/view/home_screen.dart';
+import 'package:mobile/view/playground_screen.dart';
 
 class RootScreenDestination {
   final int index;
@@ -13,6 +14,7 @@ class RootScreenDestination {
 
 final List<RootScreenDestination> rootScreenDestinations = [
   RootScreenDestination(0, 'Home', HomeScreen(), Icons.home),
+  RootScreenDestination(2, 'Playground', PlaygroundScreen(), Icons.airplay),
   RootScreenDestination(1, 'Account', AccountScreen(), Icons.account_circle),
 ];
 
