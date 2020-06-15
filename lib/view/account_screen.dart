bc10ec02e6645ed27a409e3336bf9d6e531f0c7a
Naoto Ishida <naoto.ishida@dena.com>
2020-06-15 18:50:38 +0900

    Add logout button to account screen

--- lib/view/account_screen.dart
+++ lib/view/account_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/view/mock/mock_screen_common.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile/view/onboarding/authentication_bloc.dart';
 
 class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MockScreenCommon(
-        name: 'Account screen',
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            child: const Text('Logout'),
+            onPressed: () {
+              BlocProvider.of<AuthenticationBloc>(context).add(LoggedOut());
+            },
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
