df1aaf38290bd1062cda3e024c789c801f5d7c91
Naoto Ishida <naoto.ishida@dena.com>
2020-06-16 15:09:21 +0900

    Implement auth api

---
+++ lib/api/auth_api.dart
@@ -0,0 +1,84 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+import 'package:mobile/api/api_client.dart';
+
+part 'auth_api.g.dart';
+
+abstract class AuthApi {
+  Future<String> signUp(SignUpRequestBody body);
+
+  Future<String> signIn(SignInRequestBody body);
+}
+
+class DefaultAuthApi extends AuthApi {
+  DefaultAuthApi(this._client);
+
+  final ApiClient _client;
+
+  @override
+  Future<String> signIn(SignInRequestBody body) async {
+    final response = await _client.post(
+      '/users/sign-in',
+      body: json.encode(body),
+    );
+    return AuthResponse.fromJson(jsonDecode(response.body)).token;
+  }
+
+  @override
+  Future<String> signUp(SignUpRequestBody body) async {
+    final response = await _client.post(
+      '/users/sign-up',
+      body: json.encode(body),
+    );
+    return AuthResponse.fromJson(jsonDecode(response.body)).token;
+  }
+}
+
+@JsonSerializable(includeIfNull: false)
+class SignUpRequestBody {
+  SignUpRequestBody({
+    this.name,
+    this.email,
+    this.password,
+  });
+
+  String name;
+  String email;
+  String password;
+
+  factory SignUpRequestBody.fromJson(Map<String, dynamic> json) =>
+      _$SignUpRequestBodyFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SignUpRequestBodyToJson(this);
+}
+
+@JsonSerializable(includeIfNull: false)
+class SignInRequestBody {
+  SignInRequestBody({
+    this.email,
+    this.password,
+  });
+
+  String email;
+  String password;
+
+  factory SignInRequestBody.fromJson(Map<String, dynamic> json) =>
+      _$SignInRequestBodyFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SignInRequestBodyToJson(this);
+}
+
+@JsonSerializable(includeIfNull: false)
+class AuthResponse {
+  AuthResponse({
+    this.token,
+  });
+
+  String token;
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) =>
+      _$AuthResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AuthResponseToJson(this);
+}
