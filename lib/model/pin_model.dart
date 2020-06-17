dcdc89aa7c0973d49acc23bfc34c04ec5799717a
Naoto Ishida <naoto.ishida@dena.com>
2020-06-17 15:15:57 +0900

    Use unsplash random image for mocked image url

--- lib/model/pin_model.dart
+++ lib/model/pin_model.dart
@@ -74,7 +74,7 @@ class Pin {
       url: "http://www.sample.com",
       userId: 143,
       imageUrl:
-          "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+          'https://source.unsplash.com/random/300x400',
       isPrivate: false,
     );
   }
