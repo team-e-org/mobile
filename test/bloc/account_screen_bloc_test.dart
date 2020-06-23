f28f811122b479fa5e17c8ae0678d26f5f3a3a43
Naoto Ishida <naoto.ishida@dena.com>
2020-06-23 14:41:40 +0900

    Add account screen bloc test

---
+++ test/bloc/account_screen_bloc_test.dart
@@ -0,0 +1,84 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile/api/errors/error.dart';
+import 'package:mobile/bloc/account_screen_bloc.dart';
+import 'package:mobile/data/account_repository.dart';
+import 'package:mobile/model/models.dart';
+import 'package:mobile/repository/repositories.dart';
+import 'package:mockito/mockito.dart';
+
+class MockAccountRepository extends Mock implements AccountRepository {}
+
+class MockUserRepository extends Mock implements UsersRepository {}
+
+class MockBoardRepository extends Mock implements BoardsRepository {}
+
+void main() {
+  group('AccountScreenBloc test', () {
+    MockAccountRepository accountRepository;
+    MockUserRepository userRepository;
+    MockBoardRepository boardRepository;
+    AccountScreenBloc bloc;
+
+    setUp(() {
+      accountRepository = MockAccountRepository();
+      userRepository = MockUserRepository();
+      boardRepository = MockBoardRepository();
+      bloc = AccountScreenBloc(
+        accountRepository: accountRepository,
+        usersRepository: userRepository,
+        boardsRepository: boardRepository,
+      );
+    });
+
+    test('Initial state is InitialState', () {
+      expect(bloc.initialState, equals(const InitialState()));
+      expect(bloc.state, equals(const InitialState()));
+    });
+
+    blocTest<AccountScreenBloc, AccountScreenEvent, AccountScreenState>(
+      'when fetching board data succeeded, should be default state',
+      build: () async => bloc,
+      act: (bloc) async {
+        when(accountRepository.getPersistUserId()).thenAnswer((_) => 1);
+        when(userRepository.getUser(any))
+            .thenAnswer((_) => Future.value(User.fromMock()));
+        when(userRepository.getUserBoards(any))
+            .thenAnswer((_) => Future.value([Board.fromMock()]));
+        when(boardRepository.getBoardPins(
+          id: anyNamed('id'),
+          page: anyNamed('page'),
+        )).thenAnswer((_) => Future.value([Pin.fromMock()]));
+        bloc.add(const LoadInitial());
+      },
+      expect: <dynamic>[
+        isA<Loading>(),
+        Loading(
+          user: User.fromMock(),
+          boards: const [],
+          boardPinMap: const {},
+        ),
+        DefaultState(
+          user: User.fromMock(),
+          boards: [Board.fromMock()],
+          boardPinMap: {
+            Board.fromMock().id: [Pin.fromMock()],
+          },
+        ),
+      ],
+    );
+
+    blocTest<AccountScreenBloc, AccountScreenEvent, AccountScreenState>(
+      'when fetching board data failed, should be error state',
+      build: () async => bloc,
+      act: (bloc) async {
+        when(accountRepository.getPersistUserId()).thenThrow(NetworkError());
+        bloc.add(const LoadInitial());
+      },
+      expect: <dynamic>[
+        isA<Loading>(),
+        isA<ErrorState>(),
+      ],
+    );
+  });
+}
