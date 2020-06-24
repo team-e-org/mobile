a91f825a21f5a864e9903b9e61f85e03650686c4
Naoto Ishida <naoto.ishida@dena.com>
2020-06-25 08:28:57 +0900

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ flutter build ios --release --flavor production -t lib/main-production.dart
 
 cd ios
 
-xcodebuild -workspace Runner.xcworkspace -scheme Runner -sdk iphoneos -configuration Release archive -archivePath $PWD/build/Runner.xcarchive
+xcodebuild -workspace Runner.xcworkspace -scheme Runner -sdk iphoneos -configuration "Release production" archive -archivePath $PWD/build/Runner.xcarchive
 
 xcodebuild -allowProvisioningUpdates -exportArchive -archivePath $PWD/build/Runner.xcarchive -exportOptionsPlist exportOptions.plist -exportPath $PWD/build/Runner.ipa
 
