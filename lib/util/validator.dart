ac0f24f9adb6c12ffba296b086f76b738847abc8
Naoto Ishida <naoto.ishida@dena.com>
2020-06-25 09:24:30 +0900

    Restore password validation logic

--- lib/util/validator.dart
+++ lib/util/validator.dart
@@ -9,19 +9,18 @@ class Validator {
   }
 
   static bool isValidPassword(String s) {
-    // TODO: サーバーのテストアカウントが修正されしだいコメントを戻す
-//     if (!RegExp(r"[a-z]").hasMatch(s)) {
-//       return false;
-//     }
-//     if (!RegExp(r"[A-Z]").hasMatch(s)) {
-//       return false;
-//     }
-//     if (!RegExp(r"\d").hasMatch(s)) {
-//       return false;
-//     }
-//     if (!RegExp(r"[!#%_-]").hasMatch(s)) {
-//       return false;
-//     }
+    if (!RegExp(r'[a-z]').hasMatch(s)) {
+      return false;
+    }
+    if (!RegExp(r'[A-Z]').hasMatch(s)) {
+      return false;
+    }
+    if (!RegExp(r'\d').hasMatch(s)) {
+      return false;
+    }
+    if (!RegExp(r'[!#%_-]').hasMatch(s)) {
+      return false;
+    }
 
     return true;
   }
