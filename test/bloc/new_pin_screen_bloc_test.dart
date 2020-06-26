edf70c6cce4d1f91e58a06c68c3ceaadbb6cd3b0
Naoto Ishida <naoto.ishida@dena.com>
2020-06-26 17:27:42 +0900

    Add new pin screen bloc test

---
+++ test/bloc/new_pin_screen_bloc_test.dart
@@ -0,0 +1,77 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:file/memory.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile/api/errors/error.dart';
+import 'package:mobile/bloc/new_pin_screen_bloc.dart';
+import 'package:mobile/model/models.dart';
+import 'package:mobile/repository/pins_repository.dart';
+import 'package:mockito/mockito.dart';
+
+class MockPinsRepository extends Mock implements PinsRepository {}
+
+final mockFile = MemoryFileSystem().file('image.dart')..writeAsBytesSync([0]);
+
+void main() {
+  group('new pin screen bloc test', () {
+    MockPinsRepository pinsRepository;
+    NewPinScreenBloc bloc;
+
+    setUp(() {
+      pinsRepository = MockPinsRepository();
+      bloc = NewPinScreenBloc(
+        pinsRepository: pinsRepository,
+      );
+    });
+
+    test('initial state is InitialState', () {
+      expect(bloc.initialState, isA<InitialState>());
+      expect(bloc.state, isA<InitialState>());
+    });
+
+    blocTest<NewPinScreenBloc, NewPinScreenEvent, NewPinScreenState>(
+      'when creating new pin succeeded, should be success state',
+      build: () async => bloc,
+      act: (bloc) async {
+        when(pinsRepository.createPin(any, any, any))
+            .thenAnswer((_) => Future.value());
+        bloc.add(SendRequest(
+          newPin: NewPin.fromMock(),
+          imageFile: mockFile,
+          board: Board.fromMock(),
+        ));
+      },
+      expect: <dynamic>[
+        isA<Sending>(),
+        isA<Finished>(),
+      ],
+    );
+
+    blocTest<NewPinScreenBloc, NewPinScreenEvent, NewPinScreenState>(
+      'when creating new pin failed, should be error state and it is retryable',
+      build: () async => bloc,
+      act: (bloc) async {
+        when(pinsRepository.createPin(null, null, null))
+            .thenThrow(NetworkError());
+        when(pinsRepository.createPin(NewPin.fromMock(), any, Board.fromMock()))
+            .thenAnswer((_) => Future.value());
+        bloc
+          ..add(const SendRequest(
+            newPin: null,
+            imageFile: null,
+            board: null,
+          ))
+          ..add(SendRequest(
+            newPin: NewPin.fromMock(),
+            imageFile: mockFile,
+            board: Board.fromMock(),
+          ));
+      },
+      expect: <dynamic>[
+        isA<Sending>(),
+        isA<ErrorState>(),
+        isA<Sending>(),
+        isA<Finished>(),
+      ],
+    );
+  });
+}
