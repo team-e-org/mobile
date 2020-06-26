21de89078fe94aa5f8dc6120f05f6cb1d40f83ee
Naoto Ishida <naoto.ishida@dena.com>
2020-06-26 16:54:55 +0900

    Add home screen bloc test

--- test/bloc/home_screen_bloc_test.dart
+++ test/bloc/home_screen_bloc_test.dart
@@ -1,8 +1,8 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile/api/errors/error.dart';
-import 'package:mobile/bloc/board_detail_screen_bloc.dart';
-import 'package:mobile/model/board_model.dart';
+import 'package:mobile/bloc/home_screen_bloc.dart';
+import 'package:mobile/bloc/pins_bloc.dart';
 import 'package:mobile/model/models.dart';
 import 'package:mobile/repository/pins_repository.dart';
 import 'package:mockito/mockito.dart';
@@ -10,16 +10,13 @@ import 'package:mockito/mockito.dart';
 class MockPinsRepository extends Mock implements PinsRepository {}
 
 void main() {
-  group('board detail screen bloc test', () {
+  group('home screen bloc test', () {
     MockPinsRepository pinsRepository;
-    BoardDetailScreenBloc bloc;
+    HomeScreenBloc bloc;
 
     setUp(() {
       pinsRepository = MockPinsRepository();
-      bloc = BoardDetailScreenBloc(
-        pinsRepository: pinsRepository,
-        board: Board.fromMock(),
-      );
+      bloc = HomeScreenBloc(pinsRepository);
     });
 
     test('initial state is InitialState', () {
@@ -27,15 +24,13 @@ void main() {
       expect(bloc.state, isA<InitialState>());
     });
 
-    blocTest<BoardDetailScreenBloc, BoardDetailScreenEvent,
-        BoardDetailScreenState>(
-      'when fetching board data succeeded, should be default state',
+    blocTest<HomeScreenBloc, PinsBlocEvent, PinsBlocState>(
+      'when fetching home sceen pins succeeded, should be default state',
       build: () async => bloc,
       act: (bloc) async {
-        when(pinsRepository.getBoardPins(
-                boardId: anyNamed('boardId'), page: anyNamed('page')))
+        when(pinsRepository.getHomePagePins(page: anyNamed('page')))
             .thenAnswer((_) => Future.value([Pin.fromMock()]));
-        bloc.add(LoadPinsPage());
+        bloc.add(PinsBlocEvent.loadNext);
       },
       expect: <dynamic>[
         isA<Loading>(),
@@ -43,15 +38,13 @@ void main() {
       ],
     );
 
-    blocTest<BoardDetailScreenBloc, BoardDetailScreenEvent,
-        BoardDetailScreenState>(
-      'when fetching board data failed, should be error state',
+    blocTest<HomeScreenBloc, PinsBlocEvent, PinsBlocState>(
+      'when fetching home screen pins failed, should be error state',
       build: () async => bloc,
       act: (bloc) async {
-        when(pinsRepository.getBoardPins(
-                boardId: anyNamed('boardId'), page: anyNamed('page')))
+        when(pinsRepository.getHomePagePins(page: anyNamed('page')))
             .thenThrow(NetworkError());
-        bloc.add(LoadPinsPage());
+        bloc.add(PinsBlocEvent.loadNext);
       },
       expect: <dynamic>[
         isA<Loading>(),
