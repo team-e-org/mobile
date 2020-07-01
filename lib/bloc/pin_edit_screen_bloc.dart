fc40efc33745fc46ed61b5d526c98f5e450b01c9
Daigo Hayashi <d.h.9489@gmail.com>
2020-07-01 16:31:02 +0900

    add: 大枠のbloc部分の記述

--- lib/bloc/pin_edit_screen_bloc.dart
+++ lib/bloc/pin_edit_screen_bloc.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile/model/models.dart';
 
 // event
 abstract class PinEditScreenEvent extends Equatable {
@@ -11,7 +15,17 @@ abstract class PinEditScreenEvent extends Equatable {
   List<Object> get props => [];
 }
 
-class UpdatePin extends PinEditScreenEvent {}
+class UpdatePin extends PinEditScreenEvent {
+  UpdatePin({
+    @required this.newPin,
+    @required this.imageFile,
+  });
+
+  final NewPin newPin;
+  final File imageFile;
+
+  List<Object> get props => [newPin, imageFile];
+}
 
 // state
 abstract class PinEditScreenState extends Equatable {
@@ -32,8 +46,23 @@ class UpdatePinErrorState extends PinEditScreenState {}
 // bloc
 class PinEditScreenBloc extends Bloc<PinEditScreenEvent, PinEditScreenState> {
   @override
-  PinEditScreenState get initialState => null;
+  PinEditScreenState get initialState => InitialState();
 
   @override
-  Stream<PinEditScreenState> mapEventToState(PinEditScreenEvent event) async* {}
+  Stream<PinEditScreenState> mapEventToState(PinEditScreenEvent event) async* {
+    if (event is UpdatePin) {
+      yield* mapUpdatePinToState(event);
+    }
+  }
+
+  Stream<PinEditScreenState> mapUpdatePinToState(UpdatePin event) async* {
+    if (state is! UpdatePinWaiting) {
+      yield UpdatePinWaiting();
+      try {
+        UpdatePinFinished();
+      } on Exception catch (e) {
+        yield UpdatePinErrorState();
+      }
+    }
+  }
 }
--- lib/view/pin_edit_screen.dart
+++ lib/view/pin_edit_screen.dart
@@ -2,8 +2,11 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile/bloc/pin_edit_screen_bloc.dart';
 import 'package:mobile/model/models.dart';
 import 'package:mobile/routes.dart';
+import 'package:mobile/view/components/notification.dart';
 import 'package:mobile/view/pages/pin_edit_page.dart';
 import 'package:mobile/view/select_board_screen.dart';
 
@@ -20,18 +23,34 @@ class PinEditScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => PinEditScreenBloc(),
+      child: BlocConsumer<PinEditScreenBloc, PinEditScreenState>(
+        listener: (context, state) {
+          if (state is UpdatePinFinished) {
+            Navigator.of(context).pop();
+          } else if (state is UpdatePinErrorState) {
+            PinterestNotification.showError(
+              title: 'ピンの更新に失敗しました',
+              subtitle: '時間を置いてから再度試してください',
+            );
+          }
+        },
+        builder: _contentBuilder,
+      ),
+    );
+  }
+
+  Widget _contentBuilder(BuildContext context, PinEditScreenState state) {
+    final bloc = BlocProvider.of<PinEditScreenBloc>(context);
     return PinEditPage(
       image: NetworkImage(args.pin.imageUrl),
       pin: args.pin,
       submitButtonTitle: 'Update',
-      onSubmit: _onSubmit,
-    );
-  }
-
-  void _onSubmit(BuildContext context, NewPin newPin) {
-    Navigator.of(context).pushNamed(
-      Routes.createNewPinSelectBoard,
-      arguments: SelectBoardScreenArguments(),
+      isSubmitButtonLoading: state is UpdatePinWaiting,
+      onSubmit: (context, state) {
+        bloc.add(UpdatePin());
+      },
     );
   }
 }
