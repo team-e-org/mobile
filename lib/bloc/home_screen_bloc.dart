847b788530f18b74965462ff8a1161cdd9afbaf8
Daigo Hayashi <d.h.9489@gmail.com>
2020-07-01 22:21:55 +0900

    update: blocの修正、viewへの適用

--- lib/bloc/home_screen_bloc.dart
+++ lib/bloc/home_screen_bloc.dart
@@ -1,12 +1,128 @@
-import 'package:mobile/bloc/pins_bloc.dart';
-import 'package:mobile/model/pin_model.dart';
-import 'package:mobile/repository/pins_repository.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:logger/logger.dart';
+import 'package:mobile/model/models.dart';
+import 'package:mobile/repository/repositories.dart';
 
-class HomeScreenBloc extends PinsBloc {
-  HomeScreenBloc(this.pinsRepository);
+//////// Event ////////
+
+abstract class HomeScreenEvent extends Equatable {
+  const HomeScreenEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoadRecommendPins extends HomeScreenEvent {}
+
+//////// State ////////
+abstract class HomeScreenState extends Equatable {
+  const HomeScreenState({
+    this.pins,
+    this.pagingKey,
+    this.isEndOfPins,
+    this.exception,
+  });
+
+  final List<Pin> pins;
+  final String pagingKey;
+  final bool isEndOfPins;
+  final dynamic exception;
+
+  @override
+  List<Object> get props => [pins, pagingKey, exception, isEndOfPins];
+}
+
+class InitialState extends HomeScreenState {
+  InitialState()
+      : super(
+          pins: [],
+          pagingKey: '',
+          isEndOfPins: false,
+        );
+}
+
+class DefaultState extends HomeScreenState {
+  const DefaultState({
+    @required List<Pin> pins,
+    String pagingKey,
+    bool isEndOfPins,
+  }) : super(
+          pins: pins,
+          pagingKey: pagingKey,
+          isEndOfPins: isEndOfPins,
+        );
+}
+
+class Loading extends HomeScreenState {
+  const Loading({
+    @required List<Pin> pins,
+    String pagingKey,
+    bool isEndOfPins,
+  }) : super(
+          pins: pins,
+          pagingKey: pagingKey,
+          isEndOfPins: isEndOfPins,
+        );
+}
+
+class ErrorState extends HomeScreenState {
+  const ErrorState({
+    @required List<Pin> pins,
+    String pagingKey,
+    bool isEndOfPins,
+    @required dynamic exception,
+  }) : super(
+          pins: pins,
+          pagingKey: pagingKey,
+          isEndOfPins: isEndOfPins,
+          exception: exception,
+        );
+}
+
+//////// Bloc ////////
+class HomeScreenBloc extends Bloc<HomeScreenEvent, HomeScreenState> {
+  HomeScreenBloc({this.pinsRepository});
 
   final PinsRepository pinsRepository;
 
   @override
-  Future<List<Pin>> getPins({int page}) => pinsRepository.getHomePagePins();
+  HomeScreenState get initialState => InitialState();
+
+  @override
+  Stream<HomeScreenState> mapEventToState(HomeScreenEvent event) async* {
+    if (event is LoadRecommendPins) {
+      yield* mapLoadRecommendPinsToState(event);
+    }
+  }
+
+  Stream<HomeScreenState> mapLoadRecommendPinsToState(
+      LoadRecommendPins event) async* {
+    if (state is! Loading) {
+      try {
+        yield Loading(
+          pins: state.pins,
+          pagingKey: state.pagingKey,
+          isEndOfPins: state.isEndOfPins,
+        );
+        final res =
+            await pinsRepository.getReccomendPins(pagingKey: state.pagingKey);
+        yield DefaultState(
+          pins: state.pins..addAll(res.pins),
+          pagingKey: res.pagingKey,
+          isEndOfPins: res.pagingKey == null,
+        );
+      } on Exception catch (e) {
+        Logger().e(e);
+        yield ErrorState(
+          pins: state.pins,
+          pagingKey: state.pagingKey,
+          isEndOfPins: state.isEndOfPins,
+          exception: e,
+        );
+      }
+    }
+  }
 }
