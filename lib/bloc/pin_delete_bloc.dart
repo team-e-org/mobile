c38d65d7d2adcb420cb37e457b7896ad44bafc94
Daigo Hayashi <d.h.9489@gmail.com>
2020-07-01 23:09:40 +0900

    add: blocの作成

---
+++ lib/bloc/pin_delete_bloc.dart
@@ -0,0 +1,67 @@
+import 'package:equatable/equatable.dart';
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logger/logger.dart';
+import 'package:mobile/model/models.dart';
+
+// Event
+abstract class PinDeleteEvent extends Equatable {
+  const PinDeleteEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class DeletePin extends PinDeleteEvent {
+  const DeletePin({this.pin});
+  final Pin pin;
+}
+
+// State
+abstract class PinDeleteState extends Equatable {
+  const PinDeleteState({
+    this.exception,
+  });
+
+  final dynamic exception;
+
+  @override
+  List<Object> get props => [exception];
+}
+
+class InitialState extends PinDeleteState {}
+
+class DeletePinWaiting extends PinDeleteState {}
+
+class DeletePinFinished extends PinDeleteState {}
+
+class ErrorState extends PinDeleteState {
+  const ErrorState({
+    dynamic exception,
+  }) : super(exception: exception);
+}
+
+// Bloc
+class PinDeleteBloc extends Bloc<PinDeleteEvent, PinDeleteState> {
+  @override
+  PinDeleteState get initialState => InitialState();
+
+  @override
+  Stream<PinDeleteState> mapEventToState(PinDeleteEvent event) async* {
+    if (event is DeletePin) {
+      yield* _mapDeletePinToState(event);
+    }
+  }
+
+  Stream<PinDeleteState> _mapDeletePinToState(DeletePin event) async* {
+    if (state is! DeletePinWaiting) {
+      try {
+        yield DeletePinWaiting();
+        yield DeletePinFinished();
+      } on Exception catch (e) {
+        Logger().e(e);
+        yield ErrorState(exception: e);
+      }
+    }
+  }
+}
