d656f205057ccb6f2861c16f21ab8c065eecba47
Daigo Hayashi <d.h.9489@gmail.com>
2020-07-03 01:12:37 +0900

    fix: restore backgroundcolor

--- lib/view/pinterest_application.dart
+++ lib/view/pinterest_application.dart
@@ -93,7 +93,6 @@ class PinterestApplication extends StatelessWidget {
           ),
           accentColor: Colors.red,
           buttonColor: Colors.red,
-          scaffoldBackgroundColor: Colors.black,
         ),
         onGenerateRoute: (RouteSettings settings) {
           switch (settings.name) {
